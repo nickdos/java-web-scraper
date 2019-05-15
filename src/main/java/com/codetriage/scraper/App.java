3e379688894efcf4de6bed08b96cd2a4eb91d009
Nick dos Remedios <nick.dosremedios@csiro.au>
2019-05-15 12:18:44 +1000

    AMO first changes

--- src/main/java/com/codetriage/scraper/App.java
+++ src/main/java/com/codetriage/scraper/App.java
@@ -1,7 +1,15 @@
 package com.codetriage.scraper;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,70 +20,88 @@ import org.jsoup.select.Elements;
  */
 public class App {
 
-  /**
-   * The main method of our class, which will also house the scraping
-   * functionality.
-   */
-  public static void main(String[] args) {
-
-    try {
-      /**
-       * Here we create a document object,
-       * The we use JSoup to fetch the website.
-       */
-      Document doc = Jsoup.connect("https://www.codetriage.com/?language=Java").get();
-      
-      /**
-       * With the document fetched,
-       * we use JSoup???s title() method to fetch the title
-       */ 
-      System.out.printf("\nWebsite Title: %s\n\n", doc.title());
-
-
-      // Get the list of repositories
-      Elements repositories = doc.getElementsByClass("repo-item");
-      
-      /**
-       * For each repository, extract the following information:
-       * 1. Title
-       * 2. Number of issues
-       * 3. Description
-       * 4. Full name on github
-       */
-      for (Element repository : repositories) {
-        // Extract the title
-        String repositoryTitle = repository.getElementsByClass("repo-item-title").text();
-
-        // Extract the number of issues on the repository
-        String repositoryIssues = repository.getElementsByClass("repo-item-issues").text();
-
-        // Extract the description of the repository
-        String repositoryDescription = repository.getElementsByClass("repo-item-description").text();
-
-        // Get the full name of the repository
-        String repositoryGithubName = repository.getElementsByClass("repo-item-full-name").text();
-
-        /**
-         * The reposiory full name contains brackets that we remove first
-         * before generating the valif Github link.
-         */
-        String repositoryGithubLink = "https://github.com/" + repositoryGithubName.replaceAll("[()]", "");
-
-        // Format and print the information to the console
-        System.out.println(repositoryTitle + " - " + repositoryIssues);
-        System.out.println("\t" + repositoryDescription);
-        System.out.println("\t" + repositoryGithubLink);
-        System.out.println("\n");
-
-      }
-
     /**
-     * Incase of any IO errors, we want the messages 
-     * written to the console
+     * The main method of our class, which will also house the scraping
+     * functionality.
      */
-    } catch (IOException e) {
-      e.printStackTrace();
+    public static void main(String[] args) {
+        String OUTPUT_CSV_FILE = "./amo_data.csv";
+
+        try (
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(OUTPUT_CSV_FILE));
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                        .withHeader("itemId", "family", "scientificName", "fileName", "imageThumbnailUrl"));
+        ) {
+            /**
+             * Here we create a document object,
+             * The we use JSoup to fetch the website.
+             */
+            Document doc = Jsoup.connect("https://amo.ala.org.au/main.php").timeout(10000).validateTLSCertificates(false).get();
+
+            /**
+             * With the document fetched,
+             * we use JSoup???s title() method to fetch the title
+             */
+            System.out.printf("\nWebsite Title: %s\n\n", doc.title());
+
+
+            // Get the list of repositories
+            String giAlbumSelector = ".giAlbumCell div";
+            Elements albums = doc.select(giAlbumSelector);
+            int count = 0;
+
+            /**
+             * Iterate over album divs and then scrape each album page for species pages
+             */
+            for (Element album : albums) {
+                String albumTitle = album.select("img").attr("alt");
+                String albumUrl = album.select("a").attr("href");
+
+                if (StringUtils.isNotEmpty(albumTitle) && StringUtils.isNotEmpty(albumUrl)) {
+                    System.out.println(++count + ". " + albumTitle + " - " + albumUrl);
+
+                    Document speciesDoc = Jsoup.connect("https://amo.ala.org.au/"
+                            + albumUrl).timeout(10000).validateTLSCertificates(false).get();
+                    Elements speciesPages = speciesDoc.select(giAlbumSelector);
+
+                    for (Element speciesPage : speciesPages) {
+                        String speciesTitle = speciesPage.select("img").attr("alt");
+                        String speciesUrl = speciesPage.select("a").attr("href");
+                        String speciesId = (speciesUrl.length() > 20) ? StringUtils.remove(speciesUrl.substring(15, 20), "_") : "";
+
+                        if (StringUtils.isNotEmpty(speciesTitle) && StringUtils.isNotEmpty(speciesUrl) && StringUtils.isNotEmpty(speciesId)) {
+                            System.out.println("  " + ++count + ". " + speciesTitle + " - " + speciesUrl);
+
+                            Document specimenDoc = Jsoup.connect("https://amo.ala.org.au/"
+                                    + speciesUrl).timeout(10000).validateTLSCertificates(false).get();
+                            Elements specimenItems = specimenDoc.select(".giItemCell");
+
+                            if (specimenItems.size() > 0) {
+                                for (Element specimenItem : specimenItems) {
+                                    String specimenDescription = specimenItem.select(".giDescription2").html();
+                                    String specimenImgUrl = specimenItem.select("img").attr("src");
+                                    String specimenImgFilename = specimenItem.select("img").attr("alt");
+                                    String itemId = (specimenImgUrl.length() > 50) ? StringUtils.remove(specimenImgUrl.substring(45, 50), "_") : "";
+                                    System.out.println("    " + ++count + ". " + itemId + " | " + specimenImgFilename + " | " + specimenImgUrl +" | " + specimenDescription );
+                                    //csvPrinter.printRecord("1", "Sundar Pichai ♥", "CEO", "Google");
+                                }
+                            } else {
+                                // withHeader("itemId", "family", "scientificName", "fileName", "imageThumbnailUrl")
+                                csvPrinter.printRecord(speciesId, "Sundar Pichai ♥", "CEO", "Google");
+                            }
+                        }
+                    }
+                }
+            }
+
+            /**
+             * Incase of any IO errors, we want the messages
+             * written to the console
+             */
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-  }
 }
