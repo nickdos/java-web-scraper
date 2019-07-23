8dbf98fae602e70b516f9138607a7e1a536a8f05
Nick dos Remedios <nick.dosremedios@csiro.au>
2019-07-23 11:56:16 +1000

    updates

--- src/main/java/org/ala/amo/scraper/App.java
+++ src/main/java/org/ala/amo/scraper/App.java
@@ -1,11 +1,22 @@
-package com.codetriage.scraper;
+/*
+ * Copyright (C) 2019 Atlas of Living Australia
+ * All Rights Reserved.
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
+package org.ala.amo.scraper;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
